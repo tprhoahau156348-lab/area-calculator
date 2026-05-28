2a05ea2d22ed74d74119c769dfb92504d03b12e2
Efraim156 <tprhoahau156348@gmail.com>
2026-05-28 21:10:20 +0300

    קובץ נוסף עם ההוספות בשביל הפרוייקט השני

---
+++ rectangle.py
@@ -0,0 +1,21 @@
+from shape import Shape
+
+class Rectangle(Shape):
+    def __init__(self, shape_id, width, height):
+        super().__init__(shape_id, "rectangle")
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (self.width + self.height) * 2
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "type": self.shape_type,
+            "width": self.width,
+            "height": self.height
+        }
